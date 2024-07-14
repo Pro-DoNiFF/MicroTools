ffb71defb61da3174902864bafb7a9a1873a4caf
Pro-DoNiFF <88083364+Pro-DoNiFF@users.noreply.github.com>
2024-07-14 21:32:44 +0300

    Create Client Class, new methodsn - sendRequest, which send request a url and get response

--- source/Networking/Client.h
+++ source/Networking/Client.h
@@ -10,31 +10,8 @@ namespace Networking
 	class Client
 	{
 	public:
-		Client();
-		Client(std::string url);
-		Client(std::string url, std::string args);
-		~Client();
 
-		static std::string getResponse(std::string url = NULL, std::string args = NULL);
+		static std::string getResponse(std::string url, std::string args = "");
 	private:
-		std::string MAIN_URL;
-		std::string URL_ARGUMENTS;
 	};
-}
-
-Networking::Client::Client(): MAIN_URL(""), URL_ARGUMENTS("")
-{
-}
-
-Networking::Client::Client(std::string url) : MAIN_URL(url), URL_ARGUMENTS("")
-{
-}
-
-Networking::Client::Client(std::string url, std::string args) : MAIN_URL(url), URL_ARGUMENTS(args)
-{
-}
-
-
-Networking::Client::~Client()
-{
 }
