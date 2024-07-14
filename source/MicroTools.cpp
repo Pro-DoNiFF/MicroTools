2f13756e455a2084f5047bb5d34ca23c22b1eae6
Pro-DoNiFF <88083364+Pro-DoNiFF@users.noreply.github.com>
2024-07-14 21:33:56 +0300

    Update, optimize and fixes

--- source/MicroTools.cpp
+++ source/MicroTools.cpp
@@ -1,8 +1,23 @@
 ﻿#include <iostream>
-#include <boost/archive/basic_archive.hpp>
+#include "System/Array.h"
+#include "Networking/Client.h"
+
+using SArray = System::Array<std::string>;
 
 int main(int argc, char** argv)
 {
-    std::cout << "Hello world!" << std::endl;
+    //std::string res = Networking::Client::getResponse("ip-api.com", "/json/www.google.com");
+    std::string res = "https://ip-api.com";
+    if (res.find("http") != std::string::npos)
+    {
+        std::cout << "Found: " << res.find("http") << std::endl;
+    }
+    else {
+        std::cout << "Not found" << std::endl;
+    }
+    
+
+    
+
     return 0;
 }
